7a9e388fcd379e18b8ccddd5422215dcf4aa90f4
rafaelgpaves <rafaelgp3@al.insper.edu.br>
2025-02-27 23:42:16 -0300

    feat: escanear portas UDP

--- main.py
+++ main.py
@@ -20,10 +20,32 @@ KNOWN_PORTS = {
     80: "HTTP",
     119: "NNTP",
     123: "NTP",
+    135: "Microsoft EPMAP",
+    137: "NetBIOS Name Service",
+    138: "NetBIOS Datagram Service",
+    139: "NetBIOS Session Service",
     153: "SGMP",
     156: "SQL",
     158: "DMSP",
+    161: "SNMP",
+    162: "SNMPTRAP",
+    194: "IRC",
+    199: "SMUX",
+    201: "AppleTalk Routing Maintenance",
+    213: "IPX",
+    218: "MPP",
+    220: "IMAPv3",
+    259: "ESRO",
+    264: "BGMP",
+    280: "http-mgmt",
+    369: "Rpc2portmap",
+    370: "codaauth2",
+    383: "HP data alarm manager",
+    387: "AURP",
+    401: "UPS",
+    427: "SLP",
     443: "HTTPS",
+    445: "Microsoft-DS (Directory Services)",
     3306: "MYSQL",
     3389: "RDP",
     5432: "PostreSQL"
@@ -45,8 +67,7 @@ class Scanner:
 
     def scan(self, min_port: int, max_port: int, ipv6: bool, tipo = socket.SOCK_STREAM, protocolo = None):
         if min_port < 0 or min_port > 65355 or max_port < 0 or max_port > 65355:
-            print("Insira dois numeros entre 1 e 65355\n")
-            print("AAAAAA")
+            print("Insira dois números entre 0 e 65355\n")
             return
 
         # print(socket.gethostbyaddr(self.IP))
@@ -71,16 +92,26 @@ class Scanner:
 
         for port in range(min_port, max_port+1):
             if ipv6:
-                client = socket.socket(socket.AF_INET6, tipo)
-                client.settimeout(3)
-                r = client.connect_ex((self.IPv6, port))
+                if protocolo:
+                    client = socket.socket(socket.AF_INET6, tipo, protocolo)
+                    client.settimeout(3)
+                    r = client.connect_ex((self.IPv6, port))
+                else:
+                    client = socket.socket(socket.AF_INET6, tipo)
+                    client.settimeout(3)
+                    r = client.connect_ex((self.IPv6, port))
             else:
-                client = socket.socket(socket.AF_INET, tipo)
-                client.settimeout(3)
-                r = client.connect_ex((self.IP, port))
+                if protocolo:
+                    client = socket.socket(socket.AF_INET, tipo, protocolo)
+                    client.settimeout(3)
+                    r = client.connect_ex((self.IP, port))
+                else:
+                    client = socket.socket(socket.AF_INET, tipo)
+                    client.settimeout(3)
+                    r = client.connect_ex((self.IP, port))
             if r == 0:
                 if self.portas_fechadas:
-                    print(colored(f"Porta(s) {self.portas_fechadas[0]}-{self.portas_fechadas[-1]} esta(o) fechada(s)", "red"))
+                    print(colored(f"Porta(s) {self.portas_fechadas[0]}-{self.portas_fechadas[-1]} está(ão) fechada(s)", "red"))
                 servico = KNOWN_PORTS.get(port)
                 if servico is None:
                     servico = "Serviço desconhecido"
@@ -91,7 +122,7 @@ class Scanner:
                 if r == 111 or r == 11: # Porta esta fechada
                     self.portas_fechadas.append(port)
                 else:
-                    print(f"Porta {port} esta filtrada")
+                    print(f"Porta {port} está filtrada")
             client.close()
 
     def scan_ports(self, min_port: int, max_port: int, host = None, network_scanner = False, UDP = False):
@@ -117,13 +148,12 @@ class Scanner:
                 else:
                     self.scan(min_port, max_port, True)
             else:
-                print("Escolha um numero valido\n")
+                print("Escolha um número valido\n")
             if self.portas_fechadas:
-                print(colored(f"Porta(s) {self.portas_fechadas[0]}-{self.portas_fechadas[-1]} esta(o) fechada(s)", "red"))
+                print(colored(f"Porta(s) {self.portas_fechadas[0]}-{self.portas_fechadas[-1]} está(ão) fechada(s)", "red"))
                 self.portas_fechadas = []
         except Exception as e:
-            print(e)
-            print("Insira dois numeros entre 1 e 65355\n")
+            print(f"Ocorreu um erro tentando escanear portas --> {e}\n")
 
     def banner_grab(self, port: int):
         if port not in [22, 23, 80]: # portas em que da pra fazer banner grabbing
@@ -143,7 +173,7 @@ class Scanner:
         try:
             # print(f"{self.networkIP}/{self.mascaraSubRede}")
 
-            print("\nEscolha uma opcao: ")
+            print("\nEscolha uma opção: ")
             print("1. Teste extensivo (pode demorar muito)")
             print("2. ARP (pode demandar uso de 'sudo' para rodar este arquivo)")
             escolha = input(">>> ")
@@ -172,7 +202,6 @@ class Scanner:
 
             elif escolha == "1":
                 for host in ipaddress.IPv4Network(f"{self.networkIP}/{self.mascaraSubRede}"):
-                    print(host)
                     abertas = []
                     for port in KNOWN_PORTS.keys():
                         client = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
@@ -188,12 +217,14 @@ class Scanner:
 
                     if abertas:
                         print(colored(f"Host no IP {str(host)} possui portas abertas nesta rede", on_color="on_green"))
+
+                print("")
         
         except PermissionError:
-            print("Nao foi possivel escanear a rede, tente sair e rodar o script novamente usando 'sudo python main.py'\n")
+            print("Não foi possível escanear a rede, tente sair e rodar o script novamente usando 'sudo python main.py'\n")
 
         except Exception as e:
-            print(f"Nao foi possivel escanear a rede, erro --> {e}")
+            print(f"Não foi possível escanear a rede, erro --> {e}")
 
     def run(self):
         while self.running:
@@ -215,38 +246,38 @@ class Scanner:
                 if escolha == "1":
                     IP = input("Digite o IP do host: ")
                     if IP.count(".") != 3:
-                        print("Digite um IP valido")
+                        print("Digite um IP válido")
                     else:
                         self.IP = IP
                 elif escolha == "2":
                     self.IPv6 = input("Digite o IP do host: ")
                 else:
-                    print("Escolha um numero valido\n")
+                    print("Escolha um número válido\n")
                 continue
 
             elif escolha == "2":
                 IP = input("Digite o IP da rede: ")
                 if IP.count(".") != 3:
-                    print("Digite um IP valido\n")
+                    print("Digite um IP válido\n")
                 else:
                     self.networkIP = IP
-                    mascara = input("Digite a mascara de sub-rede: ")
+                    mascara = input("Digite a máscara de sub-rede: ")
                     if mascara.isdigit() or (mascara.count(".") == 3):
                         self.mascaraSubRede = mascara
                     else:
-                        print("Digite uma mascara de sub-rede valida\n")
+                        print("Digite uma máscara de sub-rede válida\n")
                 continue 
 
             elif escolha == "3":
-                min_port = input("Escolha o range minimo para escanear: ")
-                max_port = input("Escolha o range maximo para escanear: ")
+                min_port = input("Escolha o range mínimo para escanear: ")
+                max_port = input("Escolha o range máximo para escanear: ")
                 self.scan_ports(min_port, max_port)
                 continue
 
             elif escolha == "4":
-                min_port = input("Escolha o range minimo para escanear: ")
-                max_port = input("Escolha o range maximo para escanear: ")
-                self.scan_ports(min_port, max_port)
+                min_port = input("Escolha o range mínimo para escanear: ")
+                max_port = input("Escolha o range máximo para escanear: ")
+                self.scan_ports(min_port, max_port, UDP=True)
                 continue
 
             elif escolha == "5":
@@ -256,11 +287,10 @@ class Scanner:
                         banner = self.banner_grab(port)
                         if banner:
                             print(f"Banner capturado na porta {port} --> {banner}\n")
-                    continue
                 except Exception as e:
-                    print(e)
+                    print(f"Não foi possivel identificar o OS usando banner grabbing, erro --> {e}\n")
                 if not self.achou_banner:
-                    print("Nao foi possivel identificar o OS usando banner grabbing\n")
+                    print("Não foi possivel identificar o OS usando banner grabbing\n")
                 continue
 
             elif escolha == "6":
